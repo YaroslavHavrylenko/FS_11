63c770cf86936d8a272b47dcd9cdb7c0830bc4a6
Yaroslav Havrylenko <yasx80@gmail.com>
2025-04-18 20:53:54 +0300

    HW-1 done

--- src/main/java/hw01/RandomNumbersGame.java
+++ src/main/java/hw01/RandomNumbersGame.java
@@ -13,14 +13,24 @@ public class RandomNumbersGame {
     Scanner scanner = new Scanner(in);
     String name = scanner.nextLine();
 
-    double rn = Math.random()*100 +101;
+    double rn = Math.random()*101;
     int randomNumber = (int) rn;
+    int yourVariant;
     do {
       so.print("Enter number from 0 to 100, please: ");
+      Scanner scannerNumber = new Scanner(in);
+      String inputNumber = scannerNumber.nextLine();
+      yourVariant = Integer.parseInt(inputNumber);
+      if (yourVariant < randomNumber) {
+        so.println("Your number is too small. Please, try again.");
+      } else if (yourVariant > randomNumber) {
+        so.println("Your number is too big. Please, try again.");
+      } else {
+        so.printf(" Congratulations, %s\n", name);
+        so.printf("Random number is - %d\n", randomNumber);
+      }
+    } while (randomNumber != yourVariant);
 
-
-    } while randomNumber == yourVariant;
-    so.printf(" Congratulations, %s\n", name);
     so.print("---Game over!---");
   }
 }
