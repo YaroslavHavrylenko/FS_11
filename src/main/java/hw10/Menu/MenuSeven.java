33a8019df274107c449d36a25997b0750c6ccf68
Yaroslav Havrylenko <yhavrylenko@greenpwrd.com>
2025-05-25 12:41:27 +0300

    HW-10 - Menu 1-7 done

--- src/main/java/hw10/Menu/MenuSeven.java
+++ src/main/java/hw10/Menu/MenuSeven.java
@@ -5,17 +5,17 @@ import static hw10.Menu.Menu.familyController;
 
 public class MenuSeven {
     public static void delFamilyById () {
-        int numFamily = -1;
+        int numFamily = 0;
         int quantityOfFamilies = familyController.count();
         do {
             try {
                 System.out.print("Введіть порядковий номер родини, яку ви хочете видалити: ");
                 numFamily = Integer.parseInt(scanner.nextLine());
+                familyController.deleteFamilyByIndex(numFamily - 1);
             } catch (NumberFormatException e) {
                 System.out.println("!!!Ви ввели не число!!!");
             }
+            if(numFamily <= 0 || numFamily  > quantityOfFamilies) System.out.printf("!!!Родина з порядковим номером %d відсутня в списку!!!\n", numFamily);
         } while (!(numFamily > 0) || !(numFamily  <= quantityOfFamilies));
-
-        familyController.deleteFamilyByIndex(numFamily - 1);
     }
 }
